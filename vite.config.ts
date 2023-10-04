20ac254ae179cbca896a41c952a9188cc231ef7d
zyxkad <zyxkad@gmail.com>
2023-10-04 10:04:11 -0600

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,27 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(async ({ command, mode }) => {
+  console.log(command, mode);
+  const isdev = mode === 'development';
+  const minify = isdev ?'' :'esbuild';
+
+  return {
+    plugins: [
+      vue()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    mode: mode,
+    base: '/main',
+    build: {
+      minify: minify,
+    },
+    esbuild: {
+      pure: isdev ?[] :['console.debug'],
     }
   }
 })
