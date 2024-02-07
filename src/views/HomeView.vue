a42cab83fe2cab3575f3639586a54237f4b66537
zyxkad <zyxkad@gmail.com>
2024-02-06 17:43:15 -0700

    fix date format

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -147,8 +147,8 @@ async function verifyOrLogin(): Promise<void> {
 }
 
 function formatDate(date: Date): string {
-	const month = date.getMonth().toString().padStart(2, '0')
-	const day = date.getDay().toString().padStart(2, '0')
+	const month = (date.getMonth() + 1).toString().padStart(2, '0')
+	const day = date.getDate().toString().padStart(2, '0')
 	const hour = date.getHours().toString().padStart(2, '0')
 	const minute = date.getMinutes().toString().padStart(2, '0')
 	const second = date.getSeconds().toString().padStart(2, '0')
